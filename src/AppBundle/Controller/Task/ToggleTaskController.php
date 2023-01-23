6d3325fbe5b7566ca589f40e1289e73cb29e1bc3
CHBHR <chrishb.rey@gmail.com>
2023-01-23 15:22:37 +0100

    refacto controllers, add admin security on admin routes and implement new forms and queries for different actions

---
+++ src/AppBundle/Controller/Task/ToggleTaskController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace AppBundle\Controller\Task;
+
+use AppBundle\Entity\Task;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class ToggleTaskController extends Controller
+{
+    /**
+     * @Route("/tasks/{id}/toggle", name="task_toggle")
+     */
+    public function toggleTaskAction(Task $task)
+    {
+        $task->toggle(!$task->isDone());
+        $this->getDoctrine()->getManager()->flush();
+
+        $this->addFlash('success', sprintf('La tâche %s a bien été marquée comme faite.', $task->getTitle()));
+
+        return $this->redirectToRoute('task_list');
+    }
+}
---
+++ src/AppBundle/Controller/User/ListUserController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace AppBundle\Controller\User;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class ListUserController extends Controller
+{
+    /**
+     * @Route("/admin/users", name="user_list")
+     */
+    public function listAction()
+    {
+        return $this->render('user/list.html.twig', ['users' => $this->getDoctrine()->getRepository('AppBundle:User')->findAll()]);
+    }
+
+}
